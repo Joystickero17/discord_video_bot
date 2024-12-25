e324c7d395258ecc44c3c21bf470158d1449635a
Augusto <augustocarrillo20@gmail.com>
2024-12-25 16:11:29 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import discord
+from dotenv import load_dotenv
+import os
+from utils import save_facebook_video, save_video_tiktok
+import random
+
+load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.dm_messages = True
+intents.message_content = True
+random_messages = [
+    "Ah pues ahora soy payaso tuyo",
+    "No me creas pues mmwbo",
+    "chamo ustedes tienen que estar bien ladillados",
+    "Que quieres mldto sapo"
+]
+class Client(discord.Client):
+
+    async def on_ready(self):
+        print(self.guilds)
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('!conectame'):
+            channel = message.author.voice.channel
+            voice = await channel.connect()
+        if message.content.startswith('wendy cree que ese no es diego'):
+            await message.channel.send("dejala quieta, arriba hay un dios")
+        if 'ese no es diego' in message.content.lower():
+            await message.channel.send(random.choice(random_messages))
+        if 'diego' in message.content.lower() and 'ese no es diego' not in message.content.lower():
+            await message.channel.send(random.choice(random_messages))
+        if message.content.startswith('!help'):
+            msg= "!ahpues <url> para videos de facebook\n!ahvaina <url> para videos de tiktok\nNo abusen del bot porque se arrecha y se acuesta a dormir\nSSssssSSssSSS"
+            await message.channel.send(msg)
+        if message.content.startswith('!ahpues'):
+            try:
+                name = save_facebook_video(message.content.split(' ')[1])
+                await message.channel.send("Aqui está tu video camarada", file=discord.File(name))
+                os.remove(name)
+            except Exception as e:
+                print(e)
+                await message.channel.send("Mrc que eso que te pasa!")
+        if message.content.startswith('!ahvaina'):
+            try:
+                name = save_video_tiktok(message.content.split(' ')[1])
+                await message.channel.send("Aqui está tu video camarada", file=discord.File(name))
+                os.remove(name)
+            except Exception as e:
+                print(e)
+                await message.channel.send("Mrc que eso que te pasa!")
+
+            # await message.channel.send('quetepasa!')
+
+
+client = Client(intents=intents)
+client.run(token)
---
+++ utils.py
@@ -0,0 +1,52 @@
+import requests
+import bs4
+import urllib
+from lxml.etree import parse
+from lxml import html
+
+
+import hashlib
+
+
+
+def save_facebook_video(face_url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36'
+    }
+    
+    url = "https://getmyfb.com/process"
+    response = requests.post(url, data={"id": face_url,"locale": "es"}, headers=headers)
+    root = html.fromstring(response.content)
+    tree = root.getroottree()
+    video_element = tree.xpath("/html/body/section/div[2]/div/div[2]/a")
+    url = video_element[0]
+    if url is None:
+        raise ValueError("url not found")
+    real_url = url.get("href")
+    video_response = requests.get(real_url, headers=headers)
+    name = hashlib.md5(real_url.encode('utf-8')).hexdigest()
+    with open("{}.mp4".format(name), "wb") as f:
+        f.write(video_response.content)
+
+    return "{}.mp4".format(name)
+
+
+def save_video_tiktok(video_url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36'
+    }
+    main_url = "https://tiktokio.com/#google_vignette"
+    response = requests.get(main_url, headers=headers)
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    prefix = soup.find('input', {"name": "prefix"}).get("value")
+    url = "https://tiktokio.com/api/v1/tk-htmx"
+    response = requests.post(url, data={"prefix": prefix,"vid": video_url}, headers=headers)
+    root = html.fromstring(response.content)
+    tree = root.getroottree()
+    real_url = tree.xpath("/html/div/div[1]/div/a")[0].get("href")
+    video_response = requests.get(real_url, headers=headers)
+    name = hashlib.md5(real_url.encode('utf-8')).hexdigest()
+    with open("{}.mp4".format(name), "wb") as f:
+        f.write(video_response.content)
+
+    return "{}.mp4".format(name)
