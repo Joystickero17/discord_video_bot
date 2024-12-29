b70c61f2eca6bc375cb5eedd00cf582fe206cb24
Augusto Carrillo <augustocarrillo20@gmail.com>
2024-12-29 17:41:20 -0400

    add email support for error handling and update help message for video commands

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ import random
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
+EMAIL_SUPPORT = "augustocarrillo20@gmail.com"
+
 
 intents = discord.Intents.default()
 intents.dm_messages = True
 intents.message_content = True
-random_messages = [
-    "Ah pues ahora soy payaso tuyo",
-    "No me creas pues mmwbo",
-    "chamo ustedes tienen que estar bien ladillados",
-    "Que quieres mldto sapo"
-]
 class Client(discord.Client):
 
     async def on_ready(self):
@@ -26,42 +22,33 @@ class Client(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if message.content.startswith('!conectame'):
-            channel = message.author.voice.channel
-            voice = await channel.connect()
-        if message.content.startswith('wendy cree que ese no es diego'):
-            await message.channel.send("dejala quieta, arriba hay un dios")
-        if 'ese no es diego' in message.content.lower():
-            await message.channel.send(random.choice(random_messages))
-        if 'diego' in message.content.lower() and 'ese no es diego' not in message.content.lower():
-            await message.channel.send(random.choice(random_messages))
         if message.content.startswith('!help'):
-            msg= "!ahpues <url> para videos de facebook\n!ahvaina <url> para videos de tiktok\nNo abusen del bot porque se arrecha y se acuesta a dormir\nSSssssSSssSSS"
+            msg= "!face <url> para videos de facebook o instagram\n!tk <url> para videos de tiktok\nalternativamente con !insta <url> descargas videos de instagram puede que más lento"
             await message.channel.send(msg)
-        if message.content.startswith('!ahpues'):
+        if message.content.startswith('!face') or message.content.startswith('!ahpues'):
             try:
                 name = save_facebook_video(message.content.split(' ')[1])
-                await message.channel.send("Aqui está tu video camarada", file=discord.File(name))
+                await message.channel.send("here is your video :)", file=discord.File(name))
                 os.remove(name)
             except Exception as e:
                 print(e)
-                await message.channel.send("Mrc que eso que te pasa!")
-        if message.content.startswith('!ahvaina'):
+                await message.channel.send("Ah ocurrido un error por favor contactar a {}".format(EMAIL_SUPPORT))
+        if message.content.startswith('!tk') or message.content.startswith('!ahvaina'):
             try:
                 name = save_video_tiktok(message.content.split(' ')[1])
-                await message.channel.send("Aqui está tu video camarada", file=discord.File(name))
+                await message.channel.send("here is your video :)", file=discord.File(name))
                 os.remove(name)
             except Exception as e:
                 print(e)
-                await message.channel.send("Mrc que eso que te pasa!")
-        if message.content.startswith('!semeruca'):
+                await message.channel.send("Ah ocurrido un error por favor contactar a {}".format(EMAIL_SUPPORT))
+        if message.content.startswith('!semeruca') or message.content.startswith('!insta'):
             try:
                 name = save_video_instagram(message.content.split(' ')[1])
-                await message.channel.send("Aqui está tu video camarada", file=discord.File(name))
+                await message.channel.send("here is your video :)", file=discord.File(name))
                 os.remove(name)
             except Exception as e:
                 print(e)
-                await message.channel.send("Mrc que eso que te pasa!")
+                await message.channel.send("Ah ocurrido un error por favor contactar a {}".format(EMAIL_SUPPORT))
             # await message.channel.send('quetepasa!')
 
 
